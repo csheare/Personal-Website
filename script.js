a0b6d8ebf692b67ffa6069f810c281d917a6a3a0
Courtney Shearer <csheare@clemson.edu>
2017-06-02 00:41:09 -0400

    updates

---
+++ script.js
@@ -0,0 +1,10 @@
+document.getElementById("topArrow").onclick = function(){
+	topFunction();
+}
+
+
+
+function topFunction() {
+    document.body.scrollTop = 0; // For Chrome, Safari and Opera
+    document.documentElement.scrollTop = 0; // For IE and Firefox
+}
