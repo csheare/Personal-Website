f0e55564a0e7b21c723ddedf30912e9ac4f85c93
Courtney Shearer <csheare@clemson.edu>
2017-12-22 14:40:40 -0500

    updates

--- assets/js/script.js
+++ assets/js/script.js
@@ -6,9 +6,3 @@ function topFunction() {
     document.body.scrollTop = 0; // For Chrome, Safari and Opera
     document.documentElement.scrollTop = 0; // For IE and Firefox
 }
-
-$.get("info.txt", function(data){
-
-		alert(data);
-
-	});
