f777c069378a71eb767985eab1a5cb359e5baef2
Courtney Shearer <csheare@clemson.edu>
2019-03-02 19:20:49 -0500

    some updates:

--- current/website/static/script.js
+++ current/website/static/script.js
@@ -1,6 +1,21 @@
-var canvas=document.getElementById("canvasDemo");
-var context=canvas.getContext("2d");
-context.strokeStyle="green";
-context.moveTo(0,0);
-context.lineTo(145,120);
-context.stroke();
+var things = [ 'discovery', 'adventure', 'creation', 'rock-climbing','fast running', 'nature','public policy','genetics','hackathons','sustainability','candles','reading','Bob Ross Painting','machine learning', 'National Parks', 'La Croix', 'plants','traveling','poetry','event planning','making a difference']
+
+var count = 0
+var elem = document.getElementById("things-i-like")
+var interval = setInterval(changeText,1500);
+
+function changeText(){
+    elem.innerHTML = things[count]
+    // count = count >= things.length ? 0 : count + 1
+    count++
+    if(count >= things.length){
+        count = 0
+    }
+}
+
+setTimeout(function(){
+    changeText()
+}, 4000); 
+
+
+
