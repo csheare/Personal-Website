41c391a4eafb52c4cfeb24cbb92a7d9489f7f3c5
Courtney Shearer <csheare@clemson.edu>
2019-07-16 07:24:45 -0700

    Basic Database Connection- SQLAlc to come

--- current/website/__init__.py
+++ current/website/__init__.py
@@ -1,13 +1,16 @@
-from flask import Flask,render_template
+from flask import Flask,current_app,render_template,g
 from flask_sqlalchemy import SQLAlchemy
 from website.views import business,fun,home
-
+import sqlite3
 
 def create_app():
 
     app = Flask(__name__,instance_relative_config=True)
     app.config.from_pyfile('config.py')
-    
+
+    from . import db
+    db.init_app(app)
+
     @app.route('/')
     def base():
         return render_template('base.html')
